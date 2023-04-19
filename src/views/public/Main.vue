c34e00746b1fe248efeaf1b0f155d4747ca5f5c2
AbdulloKarimov <abdullokh2020@gmail.com>
2023-04-19 11:37:12 +0600

    added language

--- src/views/public/Main.vue
+++ src/views/public/Main.vue
@@ -1,7 +1,12 @@
 <template>
- <p>
-   {{$i18n.locale}}
- </p>
+  <div class="grid grid-cols-12 gap-2 justify-items-center mt-3">
+    <div class="col-span-12 flex flex-wrap">
+       <button type="button" class="" @click="$i18n.locale = 'kg'"><img class="w-16 h-10 mr-2 mb-2 rounded-xl shadow-xl" :class="$i18n.locale == 'kg' ? 'opacity-90' : 'opacity-20'" src="@/assets/flags/kg.png"></button>
+       <button type="button" class="" @click="$i18n.locale = 'ru'"><img class="w-16 h-10 mr-2 mb-2 rounded-xl shadow-xl" :class="$i18n.locale == 'ru' ? 'opacity-90' : 'opacity-20'" src="@/assets/flags/ru.png"></button>
+       <button type="button" class="" @click="$i18n.locale = 'en'"><img class="w-16 h-10 mr-2 mb-2 rounded-xl shadow-xl" :class="$i18n.locale == 'en' ? 'opacity-90' : 'opacity-20'" src="@/assets/flags/usa.png"></button>
+    </div>
+
+  </div>
 </template>
 
 <script setup>
